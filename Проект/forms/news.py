e71abb341956b0bdc30faf0065c68fbd011691df
vkv1004 <75637047+vkv1004@users.noreply.github.com>
2021-04-24 18:09:25 +0400

    Add files via upload

---
+++ Проект/forms/news.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    key = StringField("Ключ шифрования")
+    is_private = BooleanField("Личное")
+    submit = SubmitField('Применить')
